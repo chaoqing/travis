56d18a672aaa0f719efff1ad25f2c41cc09ff1bb
nick wong <chaoqingwang@foxmail.com>
2016-04-23 01:53:39 -0700

    clone from termux/termux-api commit: 0fd66d37e3f09a0fea280b41d43dfd15b9f69b1c

---
+++ app/src/main/java/com/termux/api/TermuxApiReceiver.java
@@ -0,0 +1,93 @@
+package com.termux.api;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.Manifest;
+
+import com.termux.api.util.TermuxApiLogger;
+import com.termux.api.util.TermuxApiPermissionActivity;
+
+public class TermuxApiReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String apiMethod = intent.getStringExtra("api_method");
+        if (apiMethod == null) {
+            TermuxApiLogger.error("Missing 'api_method' extra");
+            return;
+        }
+
+        switch (apiMethod) {
+            case "BatteryStatus":
+                BatteryStatusAPI.onReceive(this, context, intent);
+                break;
+            case "CameraInfo":
+                CameraInfoAPI.onReceive(this, context, intent);
+                break;
+            case "CameraPhoto":
+                if (TermuxApiPermissionActivity.checkAndRequestPermissions(context, intent,
+                        Manifest.permission.CAMERA)) {
+                    PhotoAPI.onReceive(this, context, intent);
+                }
+                break;
+            case "Clipboard":
+                ClipboardAPI.onReceive(this, context, intent);
+                break;
+            case "ContactList":
+                if (TermuxApiPermissionActivity.checkAndRequestPermissions(context, intent,
+                        Manifest.permission.READ_CONTACTS)) {
+                    ContactListAPI.onReceive(this, context, intent);
+                }
+                break;
+            case "Dialog":
+                context.startActivity(new Intent(context, DialogActivity.class).putExtras(intent.getExtras()).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+                break;
+            case "Download":
+                DownloadAPI.onReceive(this, context, intent);
+                break;
+            case "Location":
+                if (TermuxApiPermissionActivity.checkAndRequestPermissions(context, intent,
+                        Manifest.permission.ACCESS_FINE_LOCATION)) {
+                    LocationAPI.onReceive(this, context, intent);
+                }
+                break;
+            case "Notification":
+                NotificationAPI.onReceive(this, context, intent);
+                break;
+            case "Share":
+                ShareAPI.onReceive(this, context, intent);
+                break;
+            case "SmsInbox":
+                if (TermuxApiPermissionActivity.checkAndRequestPermissions(context, intent,
+                        Manifest.permission.READ_SMS, Manifest.permission.READ_CONTACTS)) {
+                    SmsInboxAPI.onReceive(this, context, intent);
+                }
+                break;
+            case "SmsSend":
+                if (TermuxApiPermissionActivity.checkAndRequestPermissions(context, intent,
+                        Manifest.permission.SEND_SMS)) {
+                    SmsSendAPI.onReceive(this, intent);
+                }
+                break;
+            case "SpeechToText":
+                if (TermuxApiPermissionActivity.checkAndRequestPermissions(context, intent,
+                        Manifest.permission.RECORD_AUDIO)) {
+                    SpeechToTextAPI.onReceive(context, intent);
+                }
+                break;
+            case "TextToSpeech":
+                TextToSpeechAPI.onReceive(context, intent);
+                break;
+            case "Toast":
+                ToastAPI.onReceive(context, intent);
+                break;
+            case "Vibrate":
+                VibrateAPI.onReceive(this, context, intent);
+                break;
+            default:
+                TermuxApiLogger.error("Unrecognized 'api_method' extra: '" + apiMethod + "'");
+        }
+    }
+
+}
